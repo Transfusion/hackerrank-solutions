cb33a40b17dde6bd5abcb1b22898e8a02323a3b7
Transfusion <transfusian@gmail.com>
2016-06-25 01:18:15 +0800

    Add a new solution... more to come soon

---
+++ Insertion Sort - Part 1/Solution.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    
+
+    public static void insertIntoSorted(int[] ar) {
+        int last = ar[ar.length-1];
+        int i = ar.length-2;
+        while (i + 1 > 0 && last <= ar[i]){
+            ar[i+1] = ar[i--];
+            printArray(ar);
+        }
+        ar[i+1] = last;
+        printArray(ar);
+    }
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertIntoSorted(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
